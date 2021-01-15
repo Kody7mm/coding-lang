36eb58b8bd450b8b46885737acb207a2303af2e2
Kody Gordon <gordon.km89@gmail.com>
2021-01-15 13:35:51 -0800

    Add more information to HTML, CSS, JS files. Switched strategy for using information

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,31 @@
 $(document).ready(function () {
-  $("form").submit(function (event) {
-    function showValues() {
-      var fields = $(":input").serializeArray();
-      $("#results").empty();
-      jQuery.each(fields, function (i, field) {
-        $("#results").append(field.value + " ");
-      });
-    }
+  $("form").submit(function ()
+  event.preventDefault();
+  const queOne = parseInt($("input:radio[name=question1]:checked").val());
+  const queTwo = parseInt($("input:radio[name=question2]:checked").val());
+  const queThree = parseInt($("input:radio[name=question3]:checked").val());
+  const queFour = parseInt($("input:radio[name=question4]:checked").val());
+  const queFive = parseInt($("input:radio[name=question5]:checked").val());
+  const total = (questionOne + questionTwo + questionThree + questionFour + questionFive);
+  let userName = $("input#name").val(); {
 
-    $(":checkbox, :radio").click(showValues);
-    $("select").change(showValues);
-    showValues();
-    $("#queAnswer").show();
-    event.preventDefault();
+  });
+    });
+
+
+
+
+// function showValues() {
+//   var fields = $(":input").serializeArray();
+//   $("#results").empty();
+//   jQuery.each(fields, function (i, field) {
+//     $("#results").append(field.value + " ");
+//   });
+// }
+
+// $(":checkbox, :radio").click(showValues);
+// $("select").change(showValues);
+// showValues();
+event.preventDefault();
   });
 });
