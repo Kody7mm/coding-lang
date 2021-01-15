589bef8c0020ebf729bf7148c84aa4b676b3856a
Kody Gordon <gordon.km89@gmail.com>
2021-01-15 10:46:30 -0800

    Gave up on original direction and switched to Radio function. Added basic structure to continue. Left in original code for learning purposes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("form#name").submit(function(event){
     event.preventDefault();
-    const queOne = $("input#queOne").val();
-    const queTwo = $("input#queTwo").val();
-    const queThree = $("input#queThree").val();
-    const queFour = $("input#queFour").val();
-    const queFive = $("input#queFive").val();
+    // const queOne = $("input#queOne").val();
+    // const queTwo = $("input#queTwo").val();
+    // const queThree = $("input#queThree").val();
+    // const queFour = $("input#queFour").val();
+    // const queFive = $("input#queFive").val();
+    
+    const queOne = $("input:radio[name=queOne]:checked").val();
+
   
     $(".queOne").text(queOne);
     $(".queTwo").text(queTwo);
