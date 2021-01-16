5c0436e6ac53c207ccc06a6b20c2734ac22f2d55
Kody Gordon <gordon.km89@gmail.com>
2021-01-15 16:35:35 -0800

    Add my soul has been taken from my body. The NaN error has been eradicated, yet at what cost?

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,45 @@
-$(document).ready(function() {
-  $("form#selector").submit(function() {
+$(document).ready(function () {
+  $("form#formselector").submit(function (event) {
     event.preventDefault();
-    const queOne = parseInt($("input:radio[name=radio1]:checked").val());
-    const queTwo = parseInt($("input:radio[name=radio2]:checked").val());
-    const queThree = parseInt($("input:radio[name=radio3]:checked").val());
-    const queFour = parseInt($("input:radio[name=radio4]:checked").val());
-    const queFive = parseInt($("input:radio[name=radio5]:checked").val());
+    const queOne = parseInt($("input:radio[name=queOne]:checked").val());
+    const queTwo = parseInt($("input:radio[name=queTwo]:checked").val());
+    const queThree = parseInt($("input:radio[name=queThree]:checked").val());
+    const queFour = parseInt($("input:radio[name=queFour]:checked").val());
+    const queFive = parseInt($("input:radio[name=queFive]:checked").val());
+    const total = queOne + queTwo + queThree + queFour + queFive;
+    console.log(total);
 
     $("#javascript").hide();
     $("#python").hide();
     $("#ruby").hide();
-    $(".result").hide();
+    $(".results").hide();
 
-    if (qeu1 == true || que3 == true) {
+    if(total >= 9) {
       $(".answer").fadeIn();
       $("#ruby").prepend();
       $("#ruby").fadeIn();
-    } else if (que2 == true || que4 == true) {
+    }else if(total <= 8 && total > 6) {
       $(".answer").fadeIn();
       $("#javascript").fadeIn();
-    } else {
+    }else if(total < 6) {
       $(".answer").fadeIn();
       $("#python").fadeIn();
     }
+
+    // if (qeuOne == true || queThree == true) {
+    //   $(".answer").fadeIn();
+    //   $("#ruby").prepend();
+    //   $("#ruby").fadeIn();
+    // } else if (queTwo == true || queFour == true) {
+    //   $(".answer").fadeIn();
+    //   $("#javascript").fadeIn();
+    // } else {
+    //   $(".answer").fadeIn();
+    //   $("#python").fadeIn();
+    // }
   });
+});
+
+$("form#reset").submit(function(event) {
+  location.reload();
 });
